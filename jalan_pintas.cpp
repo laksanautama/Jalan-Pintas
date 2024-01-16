89d524d4b0ca2e17c00188d7993ca86748f59c15
laksanautama <kussa_laksana@uhnsugriwa.ac.id>
2024-01-16 23:55:16 +0800

    submit file cpp and pdf

---
+++ jalan_pintas.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+
+using namespace std;
+
+//----kode di atas batas ini tidak boleh diganti atau ditambah----
+
+map<string, string> path;
+
+void jalur_penting(map<string, string> path){
+
+//CODE
+
+}
+
+int main(){
+
+return 0;
+}
